30ef01a6a17a74907b285967c76f43043260e115
Drew Cook <drewcook29@gmail.com>
2021-11-05 19:13:25 -0600

    chore: exercise 8

--- 08_errors/code/errors.go
+++ 08_errors/code/errors.go
@@ -3,119 +3,79 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
+	"os"
 )
 
-func isGreaterThanTen(num int) error {
-	if num < 10 {
-		return errors.New("something bad happened")
+// This throws an error if the number is less than 10.
+func greaterThanTen(n int) error {
+	if n < 10 {
+		msg := fmt.Sprintf("ERROR: %d is not greater than 10!", n)
+		return errors.New(msg)
 	}
 	return nil
 }
 
-// func openFile() error {
-// 	f, err := os.Open("missingFile.txt")
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer f.Close()
-// 	return nil
-// }
-
-func main() {
-	num := 9
-	err := isGreaterThanTen(num)
+// This throws an error if can't find file to open
+func openFile(filename string) error {
+	f, err := os.Open(filename)
 	if err != nil {
-		fmt.Println(fmt.Errorf("%d is NOT GREATER THAN TEN", num))
-		// panic(err)
-		// log.Fatalln(err)
+		return err // error type from os.Open()
 	}
-
-	// err := openFile()
-
-	// if err != nil {
-	// 	fmt.Println(fmt.Errorf("%v", err))
-	// }
+	defer f.Close()
+	return nil
 }
 
-// TAKE A MINUTE TO REFACTOR THE ABOVE CODE TO SCOPE THE ERROR VARIABLE INTO THE IF BLOCK
+func handlePanic() string {
+	return "HANDLING THE PANIC"
+}
 
-// ****************************************************
+func recoverFromPanic() {
+	if r := recover(); r != nil {
+		fmt.Println("We panicked but everything is fine.")
+		fmt.Println("Recovery instructions:", r)
+	}
+}
 
-// PANIC & DEFER SLIDE
+func panicOnTwo() {
+	defer recoverFromPanic()
+	for i := 0; i < 5; i++ {
+		fmt.Println(i)
+		if i == 2 {
+			panic(handlePanic())
+		}
+	}
+}
 
-// ****************************************************
+func doThings() {
+  defer fmt.Println("First Line but do this last!") // multiple defers are LIFO
+  defer fmt.Println("Do this second to last!")
+  fmt.Println("Things And Stuff should happen first")
+}
 
-// package main
+func main() {
+	// Basic errors
+	// err := greaterThanTen(higher)
+	if err := greaterThanTen(10); err != nil {
+		log.Fatalln(err)
+	} else {
+		fmt.Println("All good")
+	}
 
-// import (
-// 	"fmt"
-// )
+	if err := greaterThanTen(8); err != nil {
+		fmt.Println(err) // continues to run subsequent code
+		// panic(err) // breaks the program (stops it)
+		// log.Fatalln(err) // great for logging these errors somewhere with timestamps. set up in our logging configs
+	} else {
+		fmt.Println("All good")
+	}
 
-// func doThings() {
-// 	defer fmt.Println("First Line but do this last!")
-// 	defer fmt.Println("Do this second to last!")
-// 	fmt.Println("Things And Stuff should happen first")
-// }
+	// Open File
+	if err := openFile("/missing.txt"); err != nil {
+		fmt.Println(fmt.Errorf("%v", err))
+	}
 
-// func main() {
-// 	doThings()
-// }
-
-// ****************************************************
-
-// RECOVER SLIDE
-
-// ****************************************************
-
-// package main
-
-// import (
-// 	"fmt"
-// )
-
-// func doThings() {
-// 	for i := 0; i < 5; i++ {
-// 		fmt.Println(i)
-// 		if i == 2 {
-// 			panic("PANIC!")
-// 		}
-// 	}
-// }
-
-// func main() {
-// 	doThings()
-// }
-
-// ****************************************************
-
-// package main
-
-// import (
-// 	"fmt"
-// )
-
-// func handlePanic() string {
-// 	return "HANDLING THE PANIC"
-// }
-
-// func recoverFromPanic() {
-// 	// recover() will only return a value if there has been a panic
-// 	if r := recover(); r != nil {
-// 		fmt.Println("We panicked but everything is fine.")
-// 		fmt.Println("Panic instructions received:", r)
-// 	}
-// }
-
-// func doThings() {
-// 	defer recoverFromPanic()
-// 	for i := 0; i < 5; i++ {
-// 		fmt.Println(i)
-// 		if i == 2 {
-// 			panic(handlePanic())
-// 		}
-// 	}
-// }
-
-// func main() {
-// 	doThings()
-// }
+	// Panicking
+	doThings()
+	panicOnTwo()
+}
